63fc8d5941be36ab907c494921ac8aa4fe700afe
Aktaruzzaman Pramanik <bappycseju37@gmail.com>
2022-01-05 12:56:04 +0600

    uploaded json to xml and vice versa in java

---
+++ JsonToXMLViceVersa/src/main/java/XmlToJson.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.json.JSONObject;
+import org.json.XML;
+
+public class XmlToJson {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		Path path = Paths.get("src/test/resources/XmlFile.xml");
+		 
+		String stringXml = Files.readAllLines(path).get(0);
+		         
+		JSONObject json = XML.toJSONObject(stringXml);
+		         
+		System.out.println(json);
+	}
+
+}
